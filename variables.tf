945cada3e9ff7fcfe144001b80c84491244f2f30
Bach Nguyen <69bnguyen@gmail.com>
2026-01-15 20:54:43 +0800

    add google as identity provider

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "account-id" {
   description = "The account ID to manage CloudFlare"
   type        = string
 }
+
+variable "google-client-id" {
+  description = "The client ID to set up Google OIDC"
+  type        = string
+}
+
+variable "google-client-secret" {
+  description = "The client secret to set up Google OIDC"
+  type        = string
+}
--- zero-trust.tf
+++ zero-trust.tf
@@ -4,6 +4,16 @@ resource "cloudflare_zero_trust_organization" "organization" {
   name        = "eeternalsadness.cloudflareaccess.com"
   auth_domain = "eeternalsadness.cloudflareaccess.com"
 
+  allow_authenticate_via_warp = false
+  # auto_redirect_to_identity = false
+
+  is_ui_read_only = false
+  # ui_read_only_toggle_reason = ""
+
+  # session_duration                   = "24h"
+  # user_seat_expiration_inactive_time = "730h"
+  # warp_auth_session_duration         = "24h"
+
   # custom_pages = {
   #   forbidden = "699d98642c564d2e855e9661899b7252"
   #   identity_denied = "699d98642c564d2e855e9661899b7252"
@@ -15,14 +25,14 @@ resource "cloudflare_zero_trust_organization" "organization" {
   #   logo_path = "https://example.com/logo.png"
   #   text_color = "#c5ed1b"
   # }
+}
 
-  allow_authenticate_via_warp = false
-  # auto_redirect_to_identity = false
-
-  is_ui_read_only = false
-  # ui_read_only_toggle_reason = ""
-
-  # session_duration                   = "24h"
-  # user_seat_expiration_inactive_time = "730h"
-  # warp_auth_session_duration         = "24h"
+resource "cloudflare_zero_trust_access_identity_provider" "google" {
+  config = {
+    client_id     = var.google-client-id
+    client_secret = var.google-client-secret
+  }
+  name       = "Google"
+  type       = "google"
+  account_id = var.account-id
 }
