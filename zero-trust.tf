1c218d764a2e4af8a4200e9ec1e51d67d6b1867d
Bach Nguyen <69bnguyen@gmail.com>
2026-01-15 20:27:08 +0800

    reorganize fields in zero trust org

--- zero-trust.tf
+++ zero-trust.tf
@@ -1,7 +1,28 @@
 resource "cloudflare_zero_trust_organization" "organization" {
-  zone_id                     = cloudflare_zone.zone.id
+  zone_id = cloudflare_zone.zone.id
+
+  name        = "eeternalsadness.cloudflareaccess.com"
+  auth_domain = "eeternalsadness.cloudflareaccess.com"
+
+  # custom_pages = {
+  #   forbidden = "699d98642c564d2e855e9661899b7252"
+  #   identity_denied = "699d98642c564d2e855e9661899b7252"
+  # }
+  # login_design = {
+  #   background_color = "#c5ed1b"
+  #   footer_text = "This is an example description."
+  #   header_text = "This is an example description."
+  #   logo_path = "https://example.com/logo.png"
+  #   text_color = "#c5ed1b"
+  # }
+
   allow_authenticate_via_warp = false
-  auth_domain                 = "eeternalsadness.cloudflareaccess.com"
-  is_ui_read_only             = false
-  name                        = "eeternalsadness.cloudflareaccess.com"
+  # auto_redirect_to_identity = false
+
+  is_ui_read_only = false
+  # ui_read_only_toggle_reason = ""
+
+  # session_duration                   = "24h"
+  # user_seat_expiration_inactive_time = "730h"
+  # warp_auth_session_duration         = "24h"
 }
