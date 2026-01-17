46b4528503b14dd856199d0f4bb3fabd44dfe9bb
Bach Nguyen <69bnguyen@gmail.com>
2026-01-18 00:12:30 +0800

    add zone import

--- zone.tf
+++ zone.tf
@@ -5,3 +5,8 @@ resource "cloudflare_zone" "zone" {
   name = "eeternalsadness.dev"
   type = "full"
 }
+
+import {
+  id = data.cloudflare_account.super-admin.account_id
+  to = cloudflare_zone.zone
+}
